a73ce01a29a971ba8e9d53263f5bad14d276d61e
Jihoonahn <official@jihoon.me>
2023-03-21 22:53:30 +0900

    Update JAVAScriptKit -> Tokamak

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.7
-
+// swift-tools-version:5.6
 import PackageDescription
 let package = Package(
     name: "Portfolio",
+    platforms: [.macOS(.v11), .iOS(.v13)],
     products: [
         .executable(name: "Portfolio", targets: ["Portfolio"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftwasm/JavaScriptKit.git", from: "0.18.0")
+        .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0")
     ],
     targets: [
         .executableTarget(
             name: "Portfolio",
             dependencies: [
-                .product(name: "JavaScriptKit", package: "JavaScriptKit")
+                .product(name: "TokamakShim", package: "Tokamak")
             ]),
         .testTarget(
             name: "PortfolioTests",
